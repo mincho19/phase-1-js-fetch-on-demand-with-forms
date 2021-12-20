ea9f789e262747dddd657958639ff364a409a2f2
Min Cho <minsangcho19@gmail.com>
2021-12-20 14:43:16 -0800

    commit tree 22b7d2d8edfcb7ed537d594238171a1afa2d2ad1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = event.target.children[1].value;
+        console.log(input);
+
+        fetch('http://localhost:3000/movies')
+        .this(response => response.json())
+        .this(data => {
+            console.log(data);
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
